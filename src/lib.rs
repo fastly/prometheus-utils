aef7604e164d492eb70dc6a269cec8ded00f1f3d
Aaron Turon <aturon@fastly.com>
2021-02-08 17:03:12 -0800

    Preliminary commit.
    Co-authored-by: Adam C. Foltzer <acfoltzer@fastly.com>
    Co-authored-by: iximeow <awortman@fastly.com>
    Co-authored-by: katelyn martin <kmartin@fastly.com>

---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+//! Utilities for working with Prometheus metrics in Rust
+//!
+//! This crate builds on the Promtheus crate to provide API with additional safety guardrails:
+//!
+//! * Use [`InstrumentedFuture`] to easily instrument futures with metric updates.
+//! * Use [`GuardedGauge`] to work with gauges using an RAII-style guard that decrements
+//!   the gauge upon drop.
+//! * Use [`IntCounterWithLabels`] and [`IntGaugeWithLabels`] to produce labeled Prometheus
+//!   metrics with a type-safe API.
+
+mod gauge_guard;
+mod instrumented_future;
+mod labels;
+
+pub use gauge_guard::{GaugeGuard, GenericGaugeGuard, GuardedGauge, IntGaugeGuard};
+pub use instrumented_future::{InstrumentedFuture, IntoInstrumentedFuture};
+pub use labels::{IntCounterWithLabels, IntGaugeWithLabels, LabelValues, Labels};
+
+// See `label_enum!` below; the factoring into two macros is to accomodate parsing
+// multiple kinds of visibility annotations.
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __label_enum_internal {
+    ($(#[$attr:meta])* ($($vis:tt)*) enum $N:ident { $($(#[$var_attr:meta])* $V:ident),* }) => {
+        $(#[$attr])* $($vis)* enum $N { $($(#[$var_attr])* $V),* }
+
+        impl $N {
+            /// The name of this enum variant, as a string slice.
+            pub fn as_str(&self) -> &'static str {
+                match self {
+                    $($N::$V => stringify!($V)),*
+                }
+            }
+
+            /// A vector containing one instance of each of the enum's variants.
+            pub fn all_variants() -> Vec<Self> {
+                vec![$($N::$V),*]
+            }
+        }
+    };
+}
+
+/// Declare an enum intended to be used as a Prometheus label.
+///
+/// This helper macro can only be used to define enums consisting of tags without values.
+/// Each tag corresponds to a possible Prometheus label value. The macro then generates
+/// two functions, `as_str` and `all_variants`, as described in the example below. Those
+/// funtions are intended to assist in implementing the [`Labels`] trait for a label struct
+/// that contains the enum, ensuring a consistent conversion to strings for label values,
+/// and that all possible variants are included when implementing `possible_label_values`.
+///
+/// [`Labels`]: trait.Labels.html
+///
+/// # Example
+///
+/// When using the macro, define exactly one `enum` inside, as you would normally:
+///
+/// ```ignore
+/// label_enum! {
+///     pub(crate) enum MyErrorLabel {
+///         IoError,
+///         TimeoutError,
+///         MemoryError,
+///     }   
+/// }
+/// ```
+///
+/// The macro will declare the enum exactly as provided. But in addition, it will generate
+/// two functions:
+///
+/// ```ignore
+/// impl MyErrorLabel {
+///     /// The name of this enum variant, as a string slice.
+///     pub fn as_str(&self) -> &'static str { ... }
+///
+///     /// A vector containing one instance of each of the enum's variants.
+///     pub fn all_variants() -> Vec<Self> { ... }
+/// }
+/// ```
+#[macro_export(local_inner_macros)]
+macro_rules! label_enum {
+    ($(#[$attr:meta])* enum $N:ident { $($(#[$var_attr:meta])* $V:ident),* $(,)* }) => {
+        __label_enum_internal!($(#[$attr])* () enum $N { $($(#[$var_attr])* $V),* });
+    };
+    ($(#[$attr:meta])* pub enum $N:ident { $($(#[$var_attr:meta])* $V:ident),* $(,)* }) => {
+        __label_enum_internal!($(#[$attr])* (pub) enum $N { $($(#[$var_attr])* $V),* });
+    };
+    ($(#[$attr:meta])* pub ($($vis:tt)+) enum $N:ident { $($(#[$var_attr:meta])* $V:ident),* $(,)* }) => {
+        __label_enum_internal!($(#[$attr])* (pub ($($vis)+)) enum $N { $($(#[$var_attr])* $V),* });
+    };
+}
