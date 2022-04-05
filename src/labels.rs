3b8f4ce6b9afc42647490ce6b7d915fe5d1fd0bc
Thom Mahoney <mahoneyt@gmail.com>
2022-04-05 16:02:41 -0500

    impl add, sub, inc, dec for IntGaugeWithLabels

--- src/labels.rs
+++ src/labels.rs
@@ -142,4 +142,28 @@ impl<L: Labels> IntGaugeWithLabels<L> {
             .with_label_values(&labels.label_values())
             .set(value);
     }
+
+    /// Add the value of the gauge with the provided `labels`.
+    pub fn add(&self, labels: &L, value: i64) {
+        self.metric
+            .with_label_values(&labels.label_values())
+            .add(value);
+    }
+
+    /// Subtract the value of the gauge with the provided `labels`.
+    pub fn sub(&self, labels: &L, value: i64) {
+        self.metric
+            .with_label_values(&labels.label_values())
+            .sub(value);
+    }
+
+    /// Increment the value of the gauge with the provided `labels`.
+    pub fn inc(&self, labels: &L) {
+        self.metric.with_label_values(&labels.label_values()).inc();
+    }
+
+    /// Decrement the value of the gauge with the provided `labels`.
+    pub fn dec(&self, labels: &L) {
+        self.metric.with_label_values(&labels.label_values()).dec();
+    }
 }
